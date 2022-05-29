8b00f5a3f4665d77d2a1a5c6b942e2111fffc3e2
Kevin <58088497+0x534b@users.noreply.github.com>
2022-05-28 22:24:19 -0400

    make UI responsive and access API

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ![](/images/title.png)
-# boblov
-TOHacks entry
+# 🅱️ boblov
+TOHacks entry along with [our backend api](https://github.com/0x534b/manga-translation-api)
 
 the true legend of old
+
+# ⬇️ how to install
+clone repo <br> requirements <br> do the thing 
+
+
+### featuring ratrat:
+![](/images/ratrat.png)
--- popup.js
+++ popup.js
@@ -1,61 +1,83 @@
 /* THIS IS POPUP DOT JAY ESS */
 
-// console.log(document);
+// boblov button
+/*
+const btn = document.getElementById('grabImg');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'salmon';
+  btn.style.color = 'white';
+});
+*/
 
 let grabImg = document.getElementById("grabImg");
 
-// console.log(grabImg);
-
-grabImg.addEventListener("click",() => {    
+grabImg.addEventListener("click", () => {
     // Get active browser tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         let tab = tabs[0];
         if (tab) {
             execScript(tab);
-            // grabImages();
         } else {
             alert("There are no active tabs")
         }
     })
-    // const tabId = getTabId();
-    // execScript(tabId);
 })
 
 function execScript(tab) {
     // Execute a function on a page of the current browser tab
     // and process the result of execution
-    chrome.scripting.executeScript(
-        {
-            target:{tabId: tab.id, allFrames: true},
-            func:grabImages
-        },
-        onResult
+    chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames: true },
+            func: grabImages
+        } //,
+        // onResult
     )
 }
 
-function onResult(frames) {
-    // If script execution failed on remote end 
-    // and could not return results
-    if (!frames || !frames.length) { 
-        alert("Could not retrieve images from specified page");
-        return;
-    }
-    // Combine arrays of image URLs from 
-    // each frame to a single array
-    const imageUrls = frames.map(frame=>frame.result)
-                            .reduce((r1,r2)=>r1.concat(r2));
 
-    // Open a page with a list of images and send urls to it
-    console.log(imageUrls);
-}
-
-
+// get image from current tab (senmanga only)
 function grabImages() {
-    // console.log(document);
     const images = document.querySelectorAll("img[alt*=Page]");
 
     // can change the image w smth like this
-    // images[0].src = "https://raw.senmanga.com/img/logo.svg";
+    // images[0].src = "REPLACE_URL";
 
-    return Array.from(images).map(image=>image.src);
+    let frames = Array.from(images).map(image => image.src);
+
+    // If script execution failed on remote end 
+    // and could not return results
+    if (!frames || !frames.length) {
+        alert("Could not retrieve images from specified page");
+        return;
+    }
+
+
+    // console.log(frames);
+
+    // Combine arrays of image URLs from 
+    // each frame to a single array
+    // const imageUrls = frames.map(frame => frame.result)
+    //     .reduce((r1, r2) => r1.concat(r2));
+
+    // // Open a page with a list of images and send urls to it
+    // console.log(imageUrls);
+
+    // api call to translate and typeset
+    let encode = encodeURIComponent(frames[0])
+
+    let api_url = "http://localhost:8000/"
+
+    fetch(`${api_url}?url=${encode}`)
+        .then(res => res.blob()) // Gets the response and returns it as a blob
+        .then(blob => {
+            let objectURL = URL.createObjectURL(blob);
+            images[0].src = objectURL;
+            // console.log(objectURL);
+        });
+
+    // console.log(images[0].src);
 }
+
+// blob:https://raw.senmanga.com/958d51db-879d-4a43-90e4-eebb1f816364
+// blob:https://raw.senmanga.com/a9608e1d-bffd-4974-a0f3-089ce1fd6e04
